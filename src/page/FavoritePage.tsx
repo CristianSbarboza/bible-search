f269c633838f05b02f0cc3ee3a2be0d7cc000da5
CristianSbarboza <cristianbsconcursos@gmail.com>
2025-07-19 13:44:06 -0300

    upgrade table and component conditional

--- src/page/FavoritePage.tsx
+++ src/page/FavoritePage.tsx
@@ -21,28 +21,31 @@ export function FavoritePage() {
 
   return (
     <div className="p-5 bg-[linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.7)),url('/img/backGroundBible.jpg')] bg-cover bg-center w-screen h-screen">
-      <section className='overflow-x-auto flex'>
-        <table className='text-white font-bold min-w-3xs border-collapse'>
-          <thead>
-            <tr>
-              <th className='border'>Livro</th>
-              <th className='border'>Capitulo - versiculo</th>
-              <th className='border'>Texto</th>
-            </tr>
-          </thead>
-          <tbody>
-            {verseshistory.map(verse => (
-              <tr key={verse.chapter + verse.verse} className='text-center'>
-                <td className='border p-2'>{verse.book}</td>
-                <td className='border p-2'>
-                  {verse.chapter} - {verse.verse}
-                </td>
-                <td className='border p-2'>{verse.verseText}</td>
+      {verseshistory.length === 0 && <p className='text-white'>Nenhum versiculo salvo em favoritos</p>}
+      {verseshistory.length > 0 && (
+        <section className='overflow-x-auto flex'>
+          <table className='text-white font-bold min-w-3xs border-collapse'>
+            <thead>
+              <tr>
+                <th className='border'>Livro</th>
+                <th className='border'>Capitulo - versiculo</th>
+                <th className='border'>Texto</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
+            </thead>
+            <tbody>
+              {verseshistory.map(verse => (
+                <tr key={verse.chapter + verse.verse} className='text-center'>
+                  <td className='border p-2'>{verse.book}</td>
+                  <td className='border p-2'>
+                    {verse.chapter} - {verse.verse}
+                  </td>
+                  <td className='border p-2'>{verse.verseText}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+      )}
       <button className='text-white cursor-pointer'>
         <Link to='/'>Pesquisar versiculo</Link>
       </button>
